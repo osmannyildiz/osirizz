f048539717845fecf8d519e2bd435fc3ae21b5cd
osmannyildiz <iamosmannyildiz@gmail.com>
2025-07-09 22:49:44 +0300

    add window count badge

--- src/background.ts
+++ src/background.ts
@@ -2,6 +2,8 @@ import browser from "webextension-polyfill";
 import { getSavedWindows, setSavedWindows } from "./utils/data";
 import { downloadJsonFile } from "./utils/misc";
 
+console.log("🐸 Running background script");
+
 async function exportData() {
   const savedWindows = await getSavedWindows();
 
@@ -41,6 +43,15 @@ async function restoreWindow(windowId: SavedWindowId, windowData: SavedWindow) {
   // The popup will close, so no need to bother updating the UI
 }
 
+async function updateWindowCount() {
+  const windowCount = (
+    await browser.windows.getAll({ populate: false, windowTypes: ["normal"] })
+  ).length;
+  browser.browserAction.setBadgeText({ text: windowCount.toString() });
+  browser.browserAction.setBadgeBackgroundColor({ color: "#1f1e25" });
+  browser.browserAction.setBadgeTextColor({ color: "#ffffff" });
+}
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const msg = message as Message;
   switch (msg.type) {
@@ -55,3 +66,9 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
   return true;
 });
+
+setTimeout(async () => {
+  browser.windows.onCreated.addListener(updateWindowCount);
+  browser.windows.onRemoved.addListener(updateWindowCount);
+  await updateWindowCount();
+}, 1000);
